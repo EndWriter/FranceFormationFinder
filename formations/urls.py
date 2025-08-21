81b78cda9f84eec62f9e5db2f93a5d89a66620da
EndWriter <endle.amrane@gmail.com>
2025-08-21 18:31:46 +0200

    front

--- formations/urls.py
+++ formations/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import FormationSearchView
+from .views import FormationSearchView, search_page
 
 urlpatterns = [
     path('search/', FormationSearchView.as_view(), name='formation-search'),
+    # Interface de recherche (notre front)
+    path('interface/', search_page, name='formation-interface'),
 ]
--- formations/views.py
+++ formations/views.py
@@ -1,9 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .scraper import scrape_france_travail
+from django.shortcuts import render
 
 class FormationSearchView(APIView):
 	def get(self, request):
 		keyword = request.GET.get("keyword", "")
 		results = scrape_france_travail(keyword)
 		return Response(results)
+
+# retourne notre template (page de recherche)
+def search_page(request):
+    return render(request, 'formations/search.html')
